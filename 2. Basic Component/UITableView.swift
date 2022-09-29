59729edbb9160d293552b60785271e80e13b9cc0
Fikar Suwardi <88697103+fikarsuwardi@users.noreply.github.com>
2022-09-29 09:31:42 +0700

    Update UITableView.swift

--- 2. Basic Component/UITableView.swift
+++ 2. Basic Component/UITableView.swift
@@ -34,14 +34,16 @@ class TableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     tableViewShorcut.anchor(top: searchBar.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 34, left: 18, bottom: 0, right: 17), size: .init(width: 376, height: 289))
   }
   
+ // Buat kasih action pada saat row tabel di klik
  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
      print("Num: \(indexPath.row)")
      print("Value: \(myArray[indexPath.row])")
  }
 
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return myArray.count
-  }
+ // Buat nentuin jumlah rownya
+ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     return myArray.count
+ }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
